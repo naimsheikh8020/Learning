8e74fb2c3daa1c527937f11545100097581a3ef5
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-10-27 02:43:54 +0600

    Header Setup Done

--- Thapa-Tanstack/src/components/Layout/Header.jsx
+++ Thapa-Tanstack/src/components/Layout/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
-
-export default Header
+    <header>
+      <div>
+        <NavLink to="/">ThapaReactQuery</NavLink>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/trad">FetchOld</NavLink>
+          </li>
+          <li>
+            <NavLink to="/rq"> FetchRQ </NavLink>
+          </li>
+          <li>
+            <NavLink to="/infinite"> InfiniteScroll </NavLink>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
